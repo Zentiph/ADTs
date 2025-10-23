c95333c59e424ff736296019dc0315522ce0eb4e
Gavin Borne <zentiphdev@gmail.com>
2025-10-23 12:11:05 -0400

    hide stack struct

--- stack/stack.c
+++ stack/stack.c
@@ -15,63 +15,70 @@
 
 #include "../err/err.h"
 
+typedef struct stack_s *stack_t;
+#define ADT__STACK_IMPLEMENTATION
 #include "stack.h"
 
 struct stack_s {
-   size_t size;
+   size_t capacity;
    size_t ptr;
-   int *items;
+   void **items;
 };
 
-static inline bool _stack_full(stack_t stack) {
-   return stack->ptr >= stack->size;
+static inline bool _stack_full(const stack_t stack) {
+   return stack->ptr >= stack->capacity;
 }
 
-static bool _stack_realloc(stack_t stack) {
-   stack->size *= 2;
-   stack->items = realloc(stack->items, stack->size);
-   return (stack->items != NULL);
+static void _stack_realloc(const stack_t stack) {
+   if (stack->capacity == 0) { // first alloc
+      stack->capacity = ADT_STACK_SIZE_INIT;
+      stack->items = (void **)malloc(sizeof(void *) * stack->capacity);
+
+      if (!stack->items) {
+         stack_destroy(stack);
+         error(1, "Could not allocate memory for stack");
+      }
+      return;
+   }
+
+   stack->capacity *= ADT_STACK_ALLOC_MULT;
+   void **tmp =
+      (void **)realloc(stack->items, sizeof(void *) * stack->capacity);
+   if (!tmp) {
+      stack_destroy(stack);
+      error(1, "Could not reallocate memory to expand stack size");
+   }
+
+   stack->items = tmp;
 }
 
-stack_t stack_create(void) {
+const stack_t stack_create(void) {
    stack_t stack = (stack_t)malloc(sizeof(*stack));
    if (!stack)
       return NULL;
 
-   stack->size = ADT_INITIAL_STACK_SIZE;
+   stack->capacity = 0;
    stack->ptr = 0;
-
-   stack->items = (int *)malloc(stack->size * sizeof(int));
-   if (!stack->items) {
-      free(stack);
-      return NULL;
-   }
+   stack->items = NULL;
 
    return stack;
 }
 
-void stack_destroy(stack_t stack) {
+void stack_destroy(const stack_t stack) {
    if (!stack)
       return;
    free(stack->items);
    free(stack);
 }
 
-void stack_push(stack_t stack, int item) {
-   if (_stack_full(stack)) {
-      bool success = _stack_realloc(stack);
-
-      if (!success) {
-         stack_destroy(stack);
-         error(1, "Could not push new value to stack; unable to "
-                  "reallocate more memory to stack");
-      }
-   }
+void stack_push(const stack_t stack, void *item) {
+   if (_stack_full(stack))
+      _stack_realloc(stack);
 
    stack->items[stack->ptr++] = item;
 }
 
-int stack_pop(stack_t stack) {
+void *stack_pop(const stack_t stack) {
    if (stack->ptr == 0) {
       error(1, "Cannot pop empty stack");
    }
@@ -79,10 +86,10 @@ int stack_pop(stack_t stack) {
    return stack->items[--stack->ptr];
 }
 
-void stack_clear(stack_t stack) { stack->ptr = 0; }
+void stack_clear(const stack_t stack) { stack->ptr = 0; }
 
-int stack_top(stack_t stack) { return stack->items[stack->ptr - 1]; }
+void *stack_top(const stack_t stack) { return stack->items[stack->ptr - 1]; }
 
-bool stack_empty(stack_t stack) { return stack->ptr == 0; }
+bool stack_empty(const stack_t stack) { return stack->ptr == 0; }
 
-size_t stack_length(stack_t stack) { return stack->ptr; }
+size_t stack_length(const stack_t stack) { return stack->ptr; }
--- stack/stack.h
+++ stack/stack.h
@@ -14,27 +14,35 @@
 
 #include <stdbool.h>
 
-#ifndef ADT_INITIAL_STACK_SIZE
-/// The initial size of the stack.
-#define ADT_INITIAL_STACK_SIZE 128 // 2 ^ 7
+#ifndef ADT_STACK_SIZE_INIT
+/// THe initial size of the stack.
+#define ADT_STACK_SIZE_INIT 128
 #endif
 
+#ifndef ADT_STACK_ALLOC_MULT
+/// The initial size of the stack.
+#define ADT_STACK_ALLOC_MULT 2
+#endif
+
+#ifndef ADT__STACK_IMPLEMENTATION
 /// A stack, which implements a first-in-last-out (FILO) system.
-typedef struct stack_s *stack_t;
+typedef struct {
+} *stack_t;
+#endif
 
 ///
 /// @brief Create a new stack. The stack is empty on creation.
 ///
 /// @return stack_t - The new stack, or NULL if mallocation fails.
 ///
-stack_t stack_create(void);
+const stack_t stack_create(void);
 
 ///
 /// @brief Destroy a stack.
 ///
 /// @param stack - The stack to destroy.
 ///
-void stack_destroy(stack_t stack);
+void stack_destroy(const stack_t stack);
 
 ///
 /// @brief Push an item to a stack.
@@ -42,7 +50,7 @@ void stack_destroy(stack_t stack);
 /// @param stack - The stack to push to.
 /// @param item  - The item to push.
 ///
-void stack_push(stack_t stack, int item);
+void stack_push(const stack_t stack, void *item);
 
 ///
 /// @brief Pop an item from a stack.
@@ -50,14 +58,14 @@ void stack_push(stack_t stack, int item);
 /// @param stack - The stack to pop from.
 /// @return int  - The popped item.
 ///
-int stack_pop(stack_t stack);
+void *stack_pop(const stack_t stack);
 
 ///
 /// @brief Clear a stack.
 ///
 /// @param stack - The stack to clear.
 ///
-void stack_clear(stack_t stack);
+void stack_clear(const stack_t stack);
 
 ///
 /// @brief Get the top item on a stack without popping it.
@@ -65,7 +73,7 @@ void stack_clear(stack_t stack);
 /// @param stack - The stack to view the top item of.
 /// @return int  - The top item.
 ///
-int stack_top(stack_t stack);
+void *stack_top(const stack_t stack);
 
 ///
 /// @brief Check if a stack is empty.
@@ -74,7 +82,7 @@ int stack_top(stack_t stack);
 /// @return true  - If the stack is empty.
 /// @return false - If the stack is not empty.
 ///
-bool stack_empty(stack_t stack);
+bool stack_empty(const stack_t stack);
 
 ///
 /// @brief Get the number of items in the stack.
@@ -82,6 +90,6 @@ bool stack_empty(stack_t stack);
 /// @param stack   - The stack to get the size of.
 /// @return size_t - The number of items in the stack.
 ///
-size_t stack_length(stack_t stack);
+size_t stack_length(const stack_t stack);
 
 #endif // ADT_STACK_H
